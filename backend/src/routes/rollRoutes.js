929576958b65ca80f4b840e47dbf03666287d6dc
Luc Garrabos <92851982+LGARRABOS@users.noreply.github.com>
2025-10-15 16:52:09 +0200

    purge

--- backend/src/routes/rollRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { roll } from '../controllers/rollController.js';
-import { authenticate } from '../middleware/auth.js';
-
-const router = Router();
-
-router.post('/', authenticate, roll);
-
-export default router;
